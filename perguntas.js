fb15d214208ef91a9a0d8a48d62c0e11cf418846
Williansanbri <160770144+Williansanbri@users.noreply.github.com>
2024-11-28 17:07:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma poderosa linguagem de programação que pode adicionar interatividade a um site. Foi inventado por Brendan Eich, é versátil e amigável para iniciantes.'
+)
+
+criaCartao(
+    'Física',
+    'O que é calor?',
+    'Calor é a energia transferida entre dois ou mais sistemas devido a uma diferença de temperatura entre eles.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma função afim?',
+    'Função afim é qualquer função que possua a lei de formação y = ax + b, sendo a e b números reais e a diferente de zero.'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é conceito?',
+    'Conceito é a representação mental de um objeto abstrato ou concreto.'
+)
+criaCartao(
+    'Robótica',
+    'O que é Arduino',
+    'Arduino é uma plataforma de prototipagem eletrônica open source.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é um investimento?',
+    'Investimento é qualquer valor capaz de gerar um lucro, seja em um período de curto, médio ou longo prazo.'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é satélite?',
+    'Satélite são corpos que orbitam ao redor de um corpo celeste e podem ser classificados como naturais ou artificiais.'
+)
